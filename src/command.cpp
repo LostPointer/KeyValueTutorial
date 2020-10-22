5278d68a6ae10cef1975d437b8ee794771afa5cc
Ivan aka lostpointer Khokhlov <lostpointmeister@gmail.com>
2020-10-22 03:36:57 +0300

    simple clean version

--- src/command.cpp
+++ src/command.cpp
@@ -12,35 +12,11 @@ namespace command {
 
 std::unique_ptr<BaseCommand> MakeCommand(const std::string& command_string) {
   auto splitted_command = utils::SplitString(command_string, " ");
-  std::stringstream debug;
-  debug << "debug begin\n";
-  for (const auto& i : splitted_command) {
-    debug << i << "\n";
-  }
-  debug << "end debug\n";
-  std::cout << debug.str() << "\n";
-  std::vector<std::string> args;
-  std::cout << "fofofofoof\n";
-  args.insert(args.end(), splitted_command.begin(), splitted_command.end());
-
-  std::cout << "akjdjklasjdlajdl" << std::endl;
-
-  std::stringstream debug_args;
-  debug_args << "debug args begin\n" << args.size() << "\n";
-  for (const auto& i : args) {
-    std::cout << "foo " << i << std::endl;
-    debug << i << " h\n";
-  }
-  std::cout << "exit from range-based for" << std::endl;
-  debug << "end debug args\n";
-  std::cout << debug_args.str() << "\n";
 
   if (splitted_command.size() < 1) {
     throw BadCommand("Command need have name");
   }
 
-  std::cout << "splitted_command.size() = " << splitted_command.size()
-            << std::endl;
   if (splitted_command[0] == "put" || splitted_command[0] == "p") {
     std::vector<std::string> args;
     args.insert(args.begin(), splitted_command.begin() + 1,
